3a378c94984bdf3ea88a98350cae4ede6a8ad475
salemshadfan <144166796+salemshadfan@users.noreply.github.com>
2024-02-15 21:43:03 -0700

    updated class

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -1,8 +1,73 @@
 // Location Code File
-
+import java.util.ArrayList;
 
 package edu.ucalgary.oop;
 
-public class Location {
 
+
+public class Location {
+    private String name;
+    private String address;
+    private ArrayList<DisasterVictim> occupants;
+    private ArrayList<Supply> supplies;
+
+    
+    public Location(String name, String address) {
+        this.name = name;
+        this.address = address;
+        this.occupants = new ArrayList<>();
+        this.supplies = new ArrayList<>();
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and setter for address
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    
+    public DisasterVictim[] getOccupants() {
+        return occupants.toArray(new DisasterVictim[0]);
+    }
+
+    public void setOccupants(ArrayList<DisasterVictim> occupants) {
+        this.occupants = occupants;
+    }
+
+    public void addOccupant(DisasterVictim occupant) {
+        occupants.add(occupant);
+    }
+
+    public void removeOccupant(DisasterVictim occupant) {
+        occupants.remove(occupant);
+    }
+
+    
+    public Supply[] getSupplies() {
+        return supplies.toArray(new Supply[0]);
+    }
+
+    public void setSupplies(ArrayList<Supply> supplies) {
+        this.supplies = supplies;
+    }
+
+    public void addSupply(Supply supply) {
+        supplies.add(supply);
+    }
+
+    public void removeSupply(Supply supply) {
+        supplies.remove(supply);
+    }
 }
